bcbd4c03b5dc04348d180054156521ef50891323
CristiTurcan <turcancristian02@gmail.com>
2023-11-05 21:23:50 +0200

    Optimized functions

--- script.js
+++ script.js
@@ -10,24 +10,45 @@ function Book(name, author, pages, read) {
     }
 }
 
+function createCard(b) {
+    var card = document.createElement('div');
+    card.classList.add('card');
+
+    var title = document.createElement('span');
+    var author = document.createElement('span');
+    var pages = document.createElement('span');
+    var read = document.createElement('span');
+    
+    title.innerHTML = `Title: ${b.name}`;
+    author.innerHTML = `Author: ${b.author}`;
+    pages.innerHTML = `Pages: ${b.pages}`;
+    read.innerHTML = `Read: ${b.read}`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    return card;
+}
+
+function displayLibrary(b) {
+    var card = createCard(b);
+    document.querySelector('.container').appendChild(card);
+}
+
 function addBookToLibrary(b) {
     myLibrary.push(b);
+    createCard(b);
 }
 
 const harryPotter = new Book('Harry Potter', 'JK Rowling', 321, 'yes');
 const oneMore = new Book('One More', 'Someone', 43, "no");
+const anotherOne = new Book('Another One', 'Another Someone', 432, 'yes');
+const anotherOne1 = new Book('Another One1', 'Another Someone1', 332, 'no');
 
 addBookToLibrary(harryPotter);
 addBookToLibrary(oneMore);
+addBookToLibrary(anotherOne);
+addBookToLibrary(anotherOne1);
 
-const container = document.querySelector('.container')
-
-function displayBook(b) {
-    var card = document.createElement('div');
-    card.classList.add('card');
-    card.textContent = b.info();
-    container.appendChild(card);
-}
-
-displayBook(harryPotter);
-displayBook(oneMore);
+myLibrary.forEach((b) => displayLibrary(b));
